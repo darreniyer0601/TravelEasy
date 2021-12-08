debc3a5718b72d49dc592b58de23e30383f5838b
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-12-08 13:45:39 -0500

    added login and signup functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,77 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 const { db } = require('../util/db');
+require('dotenv').config();
 
 exports.login = async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        const sql = `select * from users where email = "${email}"`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+
+            if (result.length === 0) {
+                return res.status(404).json({ msg: 'User does not exist' });
+            }
+
+            const user = result[0];
+
+            const passwordsMatch = bcrypt.compare(password, user.password);
+
+            if (!passwordsMatch) {
+                return res.status(401).json({ msg: 'Passwords do not match' });
+            }
+
+            const payload = {
+                user
+            }
+
+            jwt.sign(payload, process.env.jwtSecret, (err, token) => {
+                if (err) throw err;
+                res.status(200).json({ token });
+            })
+        })
+
+        // const passwordsMatch = bcrypt.compare(password)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server error' });
+    }
 }
 
 exports.signup = async (req, res) => {
-    const { name, password } = req.body;
+    const { email, name, password } = req.body;
 
-    const sql = `insert into users (name, password, role) values ("${name}",${password}, "customer")`;
-    db.query(sql, (err, result) => {
-        if (err) return res.status(500).json({ msg: 'Server error' });
-        console.log(result);
-        res.status(200).json();
-    })
+    try {
+        console.log('hashing password');
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        console.log(hashedPassword);
+
+        const sql = `insert into users (email, name, password, role) values ("${email}", "${name}","${hashedPassword}", "customer")`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            
+            // const payload = {
+            //     user: {
+            //         email,
+            //     }
+            // };
+
+            // jwt.sign(payload, process.env.jwtSecret, (err, token) => {
+            //     if (err) throw err;
+            //     res.status(200).json({ token });
+            // })
+
+        })
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server error' });
+    }
 }
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+// Register routes
 app.use('/api/user', userRoutes);
 
 connect().then(() => {
