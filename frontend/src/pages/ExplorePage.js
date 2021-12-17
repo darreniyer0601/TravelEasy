e0d5619d2acd1eb9e0543445616f734d1439cc56
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-12-17 18:22:48 -0500

    placeholder for displaying itenaries

--- frontend/src/pages/ExplorePage.js
+++ frontend/src/pages/ExplorePage.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import ItenaryContext from '../context/Itenary/ItenaryContext';
 
 const ExplorePage = () => {
+    const { itenaries, getItenaries } = useContext(ItenaryContext);
+
+    useEffect(() => {
+        if (itenaries.length === 0) {
+            getItenaries();
+        }
+
+        // eslint-disable-next-line
+    }, [])
+
     return (
-        <div>
-            Explore 
+        <div className='d-flex flex-row'>
+            {itenaries.map(itenary => (
+                <div className='card m-2 p-2'>
+                    <p>Origin: {itenary.origin}</p>
+                    <p>Destination: {itenary.destination}</p>
+                    <p>Travelling by {itenary.vehicle}</p>
+                    <p>Staying at: {itenary.hotel}</p>
+                    <p>Staying for {itenary.days} days</p>
+                    <p>Total Expenditures: ${itenary.price}</p>
+                </div>
+            ))}
         </div>
     )
 }
